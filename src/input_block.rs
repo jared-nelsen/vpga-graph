f089c25dd43e259dc0f2546671c291a72c0c486a
Jared Nelsen <jared.nelsen@gateless.com>
2023-10-23 16:31:46 -0400

    input block

--- src/input_block.rs
+++ src/input_block.rs
@@ -4,3 +4,19 @@ pub struct InputBlock {
     pub width: i32,
     pub pins: Vec<Uuid>,
 }
+
+impl InputBlock {
+
+    pub fn new(&self, width: i32) -> Self {
+        let mut pins = Vec::new();
+        for i in 0..width {
+            pins.push(Uuid::new_v4());
+        }
+        InputBlock { width, pins }
+    }
+
+    pub fn get_pins(&self) -> Vec<Uuid> {
+        self.pins
+    }
+
+}
