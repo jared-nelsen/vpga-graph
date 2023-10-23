9fa62357c84d7a96ba2f8f6fbab4492c0623cd3a
Jared Nelsen <jared.nelsen@gateless.com>
2023-10-23 15:54:00 -0400

    io

--- src/vpga_graph/input_block.clj
+++ src/vpga_graph/input_block.clj
@@ -1,11 +1,34 @@
 (ns vpga-graph.input-block
-  (:require [vpga-graph.util :as util]))
+  (:require [vpga-graph.util :as util]
+            [vpga-graph.input-block :as input-block]))
 
 (defrecord InputBlock [width pin-ids])
 
+(defn apply-input-to-pins
+  "Updates the input pins with the given input values"
+  [input-pins input]
+  (loop [remaining-input-pins input-pins
+         remaining-input input
+         updated-pin-map {}]
+    (if (empty? remaining-input-pins)
+      updated-pin-map
+      (let [input-pin (first remaining-input-pins)
+            input-pin-id (:id input-pin)
+            input-value (first remaining-input)
+            updated-pin (assoc input-pin :state input-value)]
+        (recur (rest remaining-input-pins) 
+               (rest remaining-input) 
+               (assoc updated-pin-map input-pin-id updated-pin))))))
+
 (defn load-input
   "Loads the given input for the Input Block into the VPGA state"
-  [vpga input-block input])
+  [vpga input-block input]
+  (let [pin-map (:pin-map vpga)
+        input-block-pin-ids (:pin-ids input-block)
+        input-pins (map #(% pin-map) input-block-pin-ids)
+        new-pin-map (apply-input-to-pins input-pins input)
+        updated-pin-map (merge pin-map new-pin-map)]
+    (assoc vpga :pin-map updated-pin-map)))
 
 (defn pin-ids
   "Returns the ids of the Input Block's pins"
--- src/vpga_graph/output_block.clj
+++ src/vpga_graph/output_block.clj
@@ -1,11 +1,16 @@
 (ns vpga-graph.output-block
-  (:require [vpga-graph.util :as util]))
+  (:require [vpga-graph.util :as util]
+            [vpga-graph.output-block :as output-block]))
 
 (defrecord OutputBlock [width pin-ids])
 
 (defn retrieve-output
   "Retrieves the output of the given Output Block from the VPGA state"
-  [vpga output-block])
+  [vpga output-block]
+  (let [pin-map (:pin-map vpga)
+        output-block-ids (:pin-ids output-block)
+        output-pin-states (map #(-> pin-map % :state) output-block-ids)]
+    (vec output-pin-states)))
 
 (defn pin-ids
   "Retrieves the pin ids from the Output Block"
