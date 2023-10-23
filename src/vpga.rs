5d5e242e2c14d3ca1ce822d0b286584df75b3253
Jared Nelsen <jared.nelsen@gateless.com>
2023-10-23 17:09:01 -0400

    vpga construction

--- src/vpga.rs
+++ src/vpga.rs
@@ -1,11 +1,13 @@
+use std::collections::HashMap;
+
 use uuid::Uuid;
 
-use crate::pin::Pin;
 use crate::vpga_spec::VPGASpec;
 use crate::input_block::InputBlock;
 use crate::output_block::OutputBlock;
-use crate::lut::LUT;
 use crate::switch_box::SwitchBox;
+use crate::lut::LUT;
+use crate::pin::Pin;
 use crate::connection::Connection;
 
 struct VPGA {
@@ -14,18 +16,28 @@ struct VPGA {
     output_block: OutputBlock,
     switch_box: SwitchBox,
     luts: Vec<LUT>,
+    pin_map: HashMap<Uuid, Pin>,
+    connection_map: HashMap<String, Connection>
 }
 
 impl VPGA {
 
-    pub fn default(self, default_spec: VPGASpec) -> Self {
-        VPGA {
-            spec: default_spec,
-            input_block: InputBlock::new(default_spec.input_block_width), 
-            output_block: OutputBlock::new(default_spec.output_block_width),
-            switch_box: SwitchBox::new(default_spec.switch_box_pin_count),
-            luts: LUT::new_n(default_spec.lut_count, default_spec.lut_width),
-        }
+    pub fn default(self, spec: VPGASpec) -> Self {
+        let input_block = InputBlock::new(spec.input_block_width);
+        let output_block = OutputBlock::new(spec.output_block_width);
+        let switch_box = SwitchBox::new(spec.switch_box_pin_count);
+        let luts = LUT::new_n(spec.lut_count, spec.lut_width);
+        let pin_map = self.generate_pin_map();
+        let connection_map = self.generate_connection_map();
+        VPGA { spec, input_block, output_block, switch_box, luts, pin_map, connection_map }
+    }
+
+    fn generate_pin_map(&self) -> HashMap<Uuid, Pin> {
+        HashMap::new()
+    }
+
+    fn generate_connection_map(&self) -> HashMap<String, Connection> {
+        HashMap::new()
     }
         
 }
