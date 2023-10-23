15747a549c73058317e96ef3ea076237de838afb
Jared Nelsen <jared.nelsen@gateless.com>
2023-10-23 11:33:58 -0400

    more

--- src/vpga_graph/vpga.clj
+++ src/vpga_graph/vpga.clj
@@ -1,12 +1,18 @@
-(ns vpga-graph.vpga)
+(ns vpga-graph.vpga
+  (:require [vpga-graph.input-block :as input-block]
+            [vpga-graph.output-block :as output-block]
+            [vpga-graph.switch-box :as switch-box]
+            [vpga-graph.lut :as lut]))
 
-(defn generate
+(defrecord VPGA [spec input-block output-block switch-box luts ids-to-pins ids-to-connections])
+
+(defn generate-VPGA
   "Generates a VPGA for the given VGPA spec"
   [vpga-spec]
-  {:spec vpga-spec
-   :input-blocks []
-   :output-blocks []
-   :switch-boxes []
-   :luts []
-   :ids-to-pins {}
-   :ids-to-connections {}})
+  (let [input-block ()
+        output-block ()
+        switch-box ()
+        luts ()
+        ids-to-pins {}
+        ids-to-connections {}]
+    (VPGA. vpga-spec input-block output-block switch-box luts ids-to-pins ids-to-connections)))
